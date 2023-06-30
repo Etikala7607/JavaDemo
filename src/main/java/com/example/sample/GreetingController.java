c3e71dad134f8ee7517725165c74bda532d21e4b
Skumar7607 <sravankumaretikala3@gmail.com>
2023-06-30 10:19:07 -0500

    Files Added

---
+++ src/main/java/com/example/sample/GreetingController.java
@@ -0,0 +1,8 @@
+package com.example.sample;
+
+
+public class GreetingController {
+    public String greeting() {
+        return "Lambda function is Executed Successfully with Version 1.0.3 !!";
+    }
+}
